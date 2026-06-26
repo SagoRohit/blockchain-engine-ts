b7f69ab3bf255d98a3946b7932f99ae13c496f3c
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-26 17:14:22 +0600

    Block modeling and validation

---
+++ src/blockchain/Block.ts
@@ -0,0 +1,62 @@
+/*
+        Block
+    ----------------------
+    Properties
+
+    timestamp
+    transactions
+    previousHash
+    hash
+    nonce
+    ----------------------
+    Methods
+
+    constructor()
+    calculateHash()
+    mine() // block knows how to search for a valid nonce
+    hasValidTransactions()  // a block is valid, if it's every transaction is valid
+
+
+    Hash = previousHash + timestamp + transactions + nonce
+*/
+
+import { Transaction } from "./Transaction";
+import { Hash } from "../crypto/hash";
+
+
+export class Block {
+    public readonly timestamp : number;
+    public readonly previousHash : string;
+    public readonly transactions : Transaction [];
+    public hash : string;
+    public nonce : number;
+
+    constructor  (
+        transactions : Transaction[],
+        prevHash : string
+    ) {
+        this.timestamp = Date.now();
+        this.previousHash = prevHash;
+        this.transactions = transactions;
+        this.nonce = 0;
+        this.hash = this.calculateHash();
+    }
+
+    public calculateHash(): string {
+        const data = 
+        this.timestamp +
+        this.previousHash +
+        JSON.stringify(this.transactions) +
+        this.nonce;
+        
+        return Hash.hash(data);
+    }
+
+    public hasValidTransactions(): boolean {
+        for(const tx of this.transactions){
+            if(!tx.isValid())
+                return false;
+        }
+        return true;
+    }
+}
